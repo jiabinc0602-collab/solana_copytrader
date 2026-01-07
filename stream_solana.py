b00c5b0e8150596732b38c9761abdf4f8a4b1b27
jiabinc0602-collab <jiabinc0602@gmail.com>
2026-01-06 20:25:42 -0500

    Made a value filter to filter bot trades to prevent rate limit disconnects from RPC

--- stream_solana.py
+++ stream_solana.py
@@ -6,8 +6,7 @@ from solders.pubkey import Pubkey
 from solders.signature import Signature
 from solana.rpc.async_api import AsyncClient
 from fetch_transaction import parse_trade
-import traceback
-import random
+import re
 
 load_dotenv()
 
@@ -46,15 +45,24 @@ def process_transaction(log_response):
         logs = log_response.result.value.logs
         is_swap = False
 
+        MIN_RAW_AMOUNT = 50000000
+        is_high_value = False
+
         for log in logs:
             if "Swap" in log: 
                 is_swap = True
-                break
+                match = re.search(r"amount_in: (\d+)", log)
+
+                if match:
+                    raw_amount = int(match.group(1))
+                    if raw_amount > MIN_RAW_AMOUNT:
+                        is_high_value = True
         
-        if is_swap:
+        if is_swap and is_high_value:
             print("!", end="", flush=True) 
             
             return str(log_response.result.value.signature)
+        return None
 
     except Exception:
         return None
@@ -73,11 +81,27 @@ async def analyze_trade(signature):
                 if tx_resp.value:
                     tx_info = parse_trade(tx_response=tx_resp)
                     
-                    if tx_info and tx_info['bought_amount'] > 0:
-                        print(f"\n💸 {tx_info['signer']} | +{tx_info['bought_amount']:.4f} {tx_info['bought_mint']}")
-                    else:
-                        print("?", end="", flush=True)
-    
+                    if tx_info:
+                        SOL_MINT = "So11111111111111111111111111111111111111112"
+                        MIN_SOL_THRESHOLD = 0.2
+                        
+                        sol_amount = 0
+                        is_whale = False
+                        
+                        if tx_info['bought_mint'] == SOL_MINT:
+                            sol_amount = tx_info['bought_amount']
+                            
+                        elif tx_info['sold_mint'] == SOL_MINT:
+                            sol_amount = tx_info['sold_amount']
+
+                        if sol_amount >= MIN_SOL_THRESHOLD:
+                            is_whale = True
+                        
+                        if is_whale:
+                            print(f"🐋 {tx_info['signer']} | Swapped {tx_info['sold_amount']:.2f} {tx_info['sold_mint']} -> {tx_info['bought_amount']:.2f} {tx_info['bought_mint']}")
+                        else:
+                            print(f"Small swap: {sol_amount:.4f} SOL", end="", flush = True)
+
         except Exception as e:
             pass
 
